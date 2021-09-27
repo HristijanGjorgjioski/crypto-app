fdfb0ebd526e526ef63a65866c48eae6cf5d5008
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 08:40:06 -0400

    WOrking on search bar

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../../services/cryptoApi'
@@ -7,8 +7,15 @@ import { Card, Col, Row, Input } from 'antd'
 const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100;
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    const [cryptos, setCryptos] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(() => {
+        const filteredData = 
+            cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+        setCryptos(filteredData)
+    }, [cryptosList, searchTerm])
     
     if(isFetching) return 'Loading...'
 
