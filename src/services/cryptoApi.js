fda69019807d06d83fe6aed66cb5706835950fbe
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 01:47:46 -0400

    WOrking on fetching data from RapidApi

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,20 @@
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/stats',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': 'a0d5aa861cmsh16f88ad471beb02p158c89jsn2ba70d20aad1'
-//     }
-//   };
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'a0d5aa861cmsh16f88ad471beb02p158c89jsn2ba70d20aad1'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
