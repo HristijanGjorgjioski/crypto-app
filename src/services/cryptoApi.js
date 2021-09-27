51d485ab72c8315b699167528ad5481024ca4b87
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 01:40:15 -0400

    Started to work on fetching data from api and implementing Redux

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'a0d5aa861cmsh16f88ad471beb02p158c89jsn2ba70d20aad1'
+//     }
+//   };
