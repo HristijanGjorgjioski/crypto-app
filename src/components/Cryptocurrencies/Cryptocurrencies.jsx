d4e1a737d2b5b2b9bc57899c69bc81d90b45f5f6
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 02:16:55 -0400

    Creating layout for Cryptocurrencies component

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../../services/cryptoApi'
+import { Card, Col, Row } from 'antd'
 
 const Cryptocurrencies = () => {
     const { data: cryptosList, isFetching } = useGetCryptosQuery()
@@ -9,9 +10,19 @@ const Cryptocurrencies = () => {
     console.log(cryptos)
 
     return (
-        <div>
-            
-        </div>
+        <>
+            <Row gutters={[ 32, 32 ]} className="crypto-card-container">
+                {cryptos.map((currency) => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} />}>
+
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
