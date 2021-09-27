438915bc5cce11405e71360fe2cfd6a3df61d362
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 09:00:26 -0400

    Creating news searching logic

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Select, Typography, Row, Avatar } from 'antd'
-import moment from 'moment' 
+import moment from 'moment'
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../../services/cryptoNewApi'
+
+const { Text, TItle } = Typography
+const { Option } = Select
+
+const News = ({ simplified }) => {
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrencies', count: simplified ? 10 : 100 })
+
     return (
         <div>
             
