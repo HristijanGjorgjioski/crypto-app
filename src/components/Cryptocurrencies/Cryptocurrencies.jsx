9e90076a4b8dc12b6f8070fbe7078ca2d68f60a4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-27 08:33:17 -0400

    FIxing some bugs and displaying more data from api

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -4,19 +4,27 @@ import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../../services/cryptoApi'
 import { Card, Col, Row } from 'antd'
 
-const Cryptocurrencies = () => {
-    const { data: cryptosList, isFetching } = useGetCryptosQuery()
+const Cryptocurrencies = ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
-    console.log(cryptos)
+    
+    if(isFetching) return 'Loading...'
 
     return (
         <>
-            <Row gutters={[ 32, 32 ]} className="crypto-card-container">
-                {cryptos.map((currency) => (
+            <Row gutter={[ 32, 32 ]} className="crypto-card-container">
+                {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                         <Link to={`/crypto/${currency.id}`}>
-                            <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" src={currency.iconUrl} />}>
-
+                            <Card 
+                                title={`${currency.rank}. ${currency.name}`} 
+                                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                                hoverable
+                            >
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {millify(currency.change)}%</p>
                             </Card>
                         </Link>
                     </Col>
