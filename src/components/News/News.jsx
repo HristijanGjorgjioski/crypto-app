3575d9846791c9f079214e15278f03c5442967c3
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-28 07:24:44 -0400

    Creating search news logic

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -7,6 +7,8 @@ import { useGetCryptoNewsQuery } from '../../services/cryptoNewApi'
 const { Text, Title } = Typography
 const { Option } = Select
 
+const demoImage = 'https://img.etimg.com/thumb/msid-84014465,width-640,resizemode-4,imgsize-25637/cryptomania-all-around.jpg'
+
 const News = ({ simplified }) => {
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrencies', count: simplified ? 6 : 12 })
 
@@ -20,7 +22,17 @@ const News = ({ simplified }) => {
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
+                                <img style={{ maxWidth: '200px', maxHeight: '100px' }} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="crypto" />
                             </div>
+                            <p>{news.description > 100 
+                                ? `${news.description.substring(0, 100)} ...`
+                                : news.description}
+                            </p>
+                            <div className="provider-container">
+                                <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                <Text className="provider-name">{news.provider[0]?.name}</Text>
+                            </div>
+                            <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                         </a>
                     </Card>
                 </Col>
