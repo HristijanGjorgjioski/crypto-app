39fbd968d547ee44d595929033c4f75e4b14e3f7
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-28 09:54:28 -0400

    Update News.jsx

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Avatar, Col, Card } from 'antd'
 import moment from 'moment'
 
 import { useGetCryptoNewsQuery } from '../../services/cryptoNewApi'
+import { useGetCryptosQuery } from '../../services/cryptoApi'
 
 const { Text, Title } = Typography
 const { Option } = Select
@@ -12,6 +13,7 @@ const demoImage = 'https://img.etimg.com/thumb/msid-84014465,width-640,resizemod
 const News = ({ simplified }) => {
     const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 })
+    const { data } = useGetCryptosQuery(100)
 
     if(!cryptoNews?.value) return 'Loading...'
 
@@ -24,10 +26,11 @@ const News = ({ simplified }) => {
                         className="select-news"
                         placeholder="Select a Crypto"
                         optionFilterProp="children"
-                        onChange={(value) => console.log(value)}
+                        onChange={(value) => setNewsCategory(value)}
                         filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
                     >
                         <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
                     </Select>
                 </Col>
             )}
