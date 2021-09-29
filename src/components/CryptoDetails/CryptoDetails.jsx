fab9db7a994696edf7412036fae2d0e8acc7899c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-29 01:35:29 -0400

    Started to work on CryptoDetails page

--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import HTMLReactParser from 'html-react-parser'
+import { useParams } from 'react-router'
+import millify from 'millify'
+import { Col, Row, Typography, Select } from 'antd'
+
+const { Title, Text } = Typography
+const { Option } = Select
 
 const CryptoDetails = () => {
     return (
