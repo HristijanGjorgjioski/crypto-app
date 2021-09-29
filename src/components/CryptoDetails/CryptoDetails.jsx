5e68dc43417f6a2ea41f9d3997625e7e1fac5539
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-09-29 02:14:32 -0400

    Successfuly displaying crypto details, now is time to make chart to work

--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -18,7 +18,7 @@ const CryptoDetails = () => {
     const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod })
     const cryptoDetails = data?.data?.coin
 
-    if (isFetching) return <Loader />
+    if (isFetching) return 'Loading...'
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y']
 
