0faf3c3ceda930e4a5dc3a51870066d074db1404
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-01 09:17:15 -0400

    Started working on Exchanges component and created redux endpoint for it

--- src/components/Exchanges/Exchanges.jsx
+++ src/components/Exchanges/Exchanges.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
 
 const Exchanges = () => {
     return (
